d2b865dd0e052b2657484d78d10879e0be013a23
Roman Horbovyi <rhorbovyi@gmail.com>
2021-09-13 13:10:35 +0300

    Changes

--- greeting-service/src/main/java/rhorbovyi/greetingservice/GreetingServiceApplication.java
+++ greeting-service/src/main/java/rhorbovyi/greetingservice/GreetingServiceApplication.java
@@ -1,10 +1,22 @@
 package rhorbovyi.greetingservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GreetingServiceApplication {
+
+	@Value("${messages.greeting}")
+	private String greeting;
+
+	@GetMapping("/greeting")
+	public String greetings() {
+		return greeting;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingServiceApplication.class, args);
