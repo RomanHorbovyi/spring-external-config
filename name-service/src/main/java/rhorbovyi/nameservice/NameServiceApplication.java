b0d838714efb0704d1162562df99242f3fba1aa3
Roman Horbovyi <rhorbovyi@gmail.com>
2021-09-16 11:48:02 +0300

    More changes

--- name-service/src/main/java/rhorbovyi/nameservice/NameServiceApplication.java
+++ name-service/src/main/java/rhorbovyi/nameservice/NameServiceApplication.java
@@ -1,25 +1,26 @@
-package rhorbovyi.greetingservice;
+package rhorbovyi.nameservice;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class GreetingServiceApplication {
+public class NameServiceApplication {
 
-	@Value("${messages.greeting}")
-	private String greeting;
+	@Value("${messages.name}")
+	private String name;
 
-	@GetMapping("/greeting")
-	public String greetings() {
-		return greeting;
+	@GetMapping("/name/{userId}")
+	public String getName(@PathVariable("userId") String userId) {
+		return name;
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(GreetingServiceApplication.class, args);
+		SpringApplication.run(NameServiceApplication.class, args);
 	}
 
 }
